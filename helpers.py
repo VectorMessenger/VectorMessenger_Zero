0057726cf78f9a3b33865e09c6b2a05d2bed3690
maximilionus <xxxarakxxx@gmail.com>
2020-05-02 20:49:47 +0300

    Update chat test for client ui

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 import tkinter
 from datetime import datetime
 import sys
+from random import choice as RChoice
 
 # CONSTS
 VERSION = f"dev#{datetime.now().strftime('%d%m%Y')}"
@@ -17,10 +18,9 @@ def _testMessageBox(ui_object: object):
 	""" Will test chat messages widget by sending test messages to it """
 	from time import sleep
 	ui_object.createLog('Messagebox test begin')
-	arrayMessage = ('test_0: Все понятно, автор запустил тест месседжбокса.\n', 'test_34: Мда, вот это неожиданная ситуация конечно.\n', 'test_12: Да реально.\n', 'test_2: Застал в расплох так сказать.\n')
-	for i in range(40):	
-		for i in range(len(arrayMessage)):
-			ui_object.showMessage(arrayMessage[i])
-			ui_object.createLog('test message sent')
-			sleep(0.05)
+	arrayMessage = ('test_0: Guys, Im testing this new chat app now.\n', 'test_34: Wow! Thats cool.\n', 'test_12: Hello world!\n', 'test_2: Why? Just... why?\n')
+	for i in range(48):	
+		ui_object.showMessage(RChoice(arrayMessage))
+		ui_object.createLog('test message sent')
+		sleep(0.05)
 	ui_object.createLog('Messagebox test ended')
