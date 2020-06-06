2cc96fe8a46ba2aa877b06dd6984ad2006456234
maximilionus <xxxarakxxx@gmail.com>
2020-06-06 03:14:05 +0300

    Version upgrade

--- VectorMessenger/helpers.py
+++ VectorMessenger/helpers.py
@@ -6,7 +6,7 @@ from urllib import request
 
 
 # CONSTS
-VERSION = "B:260520200324"
+VERSION = "B:202006060312"
 VERSION_UPDATE_API = "https://docs.google.com/document/d/1jFWDZzJEPdsjs3JqcVKMfRzaFuz8VTrDc15JxsUJRUA/export?format=txt"
 ICON_CLIENT_PATH = './data/ico/VMClient.ico'
 ICON_SERVER_PATH = './data/ico/VMServer.ico'
