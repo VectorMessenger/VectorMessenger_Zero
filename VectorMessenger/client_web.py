092c3679c7afb2a32edc89668e913e53c5bc9a1a
maximilionus <xxxarakxxx@gmail.com>
2020-08-08 23:57:03 +0300

    Basic flask server implementation

--- VectorMessenger/client_web.py
+++ VectorMessenger/client_web.py
@@ -1,12 +1,29 @@
 import webview
 from os import chdir, path
 
+from flask import Flask, render_template
+
 from VectorMessenger.MessengerCore.Helpers import Global as h
 
 
+def preinit_gui_server():
+    STATIC_PATH = path.abspath('./data/src/')
+    TEMPLATES_PATH = STATIC_PATH
+
+    server = Flask(__name__, static_folder=STATIC_PATH, template_folder=TEMPLATES_PATH)
+    server.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+
+    @server.route('/')
+    def main_gui():
+        return render_template('index.html', token=webview.token)
+
+    return server
+
+
 def startup():
-    webview.create_window(h.APPDICT["client"]["title"], 'data/src/index.html')
-    webview.start(http_server=True)
+    gui_server = preinit_gui_server()
+    webview.create_window(h.APPDICT["client"]["title"], gui_server)
+    webview.start()
 
 
 def run_source():
