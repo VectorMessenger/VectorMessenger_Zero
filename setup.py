94ad993850280b7714f42e8243762d2a1bc2e8d1
maximilionus <xxxarakxxx@gmail.com>
2020-08-10 20:39:08 +0300

    Add setup script and changed name in pyproject

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='vector_messenger',
+    version='0.0.0',
+    author='maximilionus',
+    packages=['VectorMessenger'],
+    url='https://github.com/maximilionus/VectorMessenger',
+    python_requires='>=3.7, <3.8'
+)
